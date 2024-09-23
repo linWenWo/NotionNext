08d05e241af162ae214b5582500fafce521090d5
StoksY <140773561+StoksY@users.noreply.github.com>
2024-09-23 19:45:28 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export async function getStaticProps(req) {
       if (post.password && post.password !== '') {
         continue
       }
-      post.blockMap = await getPostBlocks(post.id, 'slug', POST_PREVIEW_LINES)
+      post.blockMap = await getPostBlocks(post.id, 'Multi-select', POST_PREVIEW_LINES)
     }
   }
 
